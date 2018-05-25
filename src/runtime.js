8791c1085cf3f23418335c0e5aff97e804ed277f
Rafael Fernandes Lopes <rafael.fernandes@gmail.com>
2018-05-25 18:42:17 -0300

    Initial commit

---
+++ src/runtime.js
@@ -0,0 +1,250 @@
+﻿// ECMAScript 5 strict mode
+"use strict";
+
+assert2(cr, "cr namespace not created");
+assert2(cr.plugins_, "cr.plugins_ not created");
+
+/////////////////////////////////////
+// Plugin class
+// *** CHANGE THE PLUGIN ID HERE *** - must match the "id" property in edittime.js
+//          vvvvvvvv
+cr.plugins_.SimpleQRScanner = function(runtime)
+{
+	this.runtime = runtime;
+};
+
+(function ()
+{
+	/////////////////////////////////////
+	// *** CHANGE THE PLUGIN ID HERE *** - must match the "id" property in edittime.js
+	//                            vvvvvvvv
+	var pluginProto = cr.plugins_.SimpleQRScanner.prototype;
+		
+	/////////////////////////////////////
+	// Object type class
+	pluginProto.Type = function(plugin)
+	{
+		this.plugin = plugin;
+		this.runtime = plugin.runtime;
+	};
+
+	var typeProto = pluginProto.Type.prototype;
+
+	// called on startup for each object type
+	typeProto.onCreate = function()
+	{
+	};
+
+	/////////////////////////////////////
+	// Instance class
+	pluginProto.Instance = function(type)
+	{
+		this.type = type;
+		this.runtime = type.runtime;
+		
+		// any other properties you need, e.g...
+		// this.myValue = 0;
+	};
+	
+	var instanceProto = pluginProto.Instance.prototype;
+
+	// called whenever an instance is created
+	instanceProto.onCreate = function()
+	{
+		// note the object is sealed after this call; ensure any properties you'll ever need are set on the object
+		// e.g...
+		// this.myValue = 0;
+
+		//this.qrdecode = qrcode;
+		
+		this.result = "";
+
+		//var plugin = this;
+		
+		//qrcode.callback = read;
+		//this.qrdecode["callback"] = function(res){
+		//	plugin.result = res;
+		//	plugin.runtime.trigger(cr.plugins_.SimpleQRScanner.prototype.cnds.onDecoded, plugin);
+
+	};
+	
+	// called whenever an instance is destroyed
+	// note the runtime may keep the object after this call for recycling; be sure
+	// to release/recycle/reset any references to other objects in this function.
+	instanceProto.onDestroy = function ()
+	{
+	};
+	
+	// called when saving the full state of the game
+	instanceProto.saveToJSON = function ()
+	{
+		// return a Javascript object containing information about your object's state
+		// note you MUST use double-quote syntax (e.g. "property": value) to prevent
+		// Closure Compiler renaming and breaking the save format
+		return {
+			// e.g.
+			//"myValue": this.myValue
+		};
+	};
+	
+	// called when loading the full state of the game
+	instanceProto.loadFromJSON = function (o)
+	{
+		// load from the state previously saved by saveToJSON
+		// 'o' provides the same object that you saved, e.g.
+		// this.myValue = o["myValue"];
+		// note you MUST use double-quote syntax (e.g. o["property"]) to prevent
+		// Closure Compiler renaming and breaking the save format
+	};
+	
+	// only called if a layout object - draw to a canvas 2D context
+	instanceProto.draw = function(ctx)
+	{
+	};
+	
+	// only called if a layout object in WebGL mode - draw to the WebGL context
+	// 'glw' is not a WebGL context, it's a wrapper - you can find its methods in GLWrap.js in the install
+	// directory or just copy what other plugins do.
+	instanceProto.drawGL = function (glw)
+	{
+	};
+	
+	// The comments around these functions ensure they are removed when exporting, since the
+	// debugger code is no longer relevant after publishing.
+	/**BEGIN-PREVIEWONLY**/
+	instanceProto.getDebuggerValues = function (propsections)
+	{
+		// Append to propsections any debugger sections you want to appear.
+		// Each section is an object with two members: "title" and "properties".
+		// "properties" is an array of individual debugger properties to display
+		// with their name and value, and some other optional settings.
+		propsections.push({
+			"title": "My debugger section",
+			"properties": [
+				// Each property entry can use the following values:
+				// "name" (required): name of the property (must be unique within this section)
+				// "value" (required): a boolean, number or string for the value
+				// "html" (optional, default false): set to true to interpret the name and value
+				//									 as HTML strings rather than simple plain text
+				// "readonly" (optional, default false): set to true to disable editing the property
+				
+				// Example:
+				// {"name": "My property", "value": this.myValue}
+			]
+		});
+	};
+	
+	instanceProto.onDebugValueEdited = function (header, name, value)
+	{
+		// Called when a non-readonly property has been edited in the debugger. Usually you only
+		// will need 'name' (the property name) and 'value', but you can also use 'header' (the
+		// header title for the section) to distinguish properties with the same name.
+		if (name === "My property")
+			this.myProperty = value;
+	};
+	/**END-PREVIEWONLY**/
+
+	//////////////////////////////////////
+	// Conditions
+	function Cnds() {};
+
+	// the example condition
+	/*Cnds.prototype.MyCondition = function (myparam)
+	{
+		// return true if number is positive
+		return myparam >= 0;
+	};*/
+	
+	// ... other conditions here ...
+	
+	pluginProto.cnds = new Cnds();
+
+	var cnds = pluginProto.cnds;
+		
+	cnds.onDecoded = function ()
+	{
+		// ... see other behaviors for example implementations ...
+		//C
+		return true;
+	};
+	
+	//////////////////////////////////////
+	// Actions
+	function Acts() {};
+
+	// the example action
+	/*Acts.prototype.MyAction = function (myparam)
+	{
+		// alert the message
+		alert(myparam);
+	};*/
+	
+	// ... other actions here ...
+
+	Acts.prototype.qrDecode = function ()
+	{
+		var self = this;
+
+		cordova.plugins.barcodeScanner.scan(
+			function (result) {
+				var inst = self;
+
+				inst.result = result.text;
+
+				alert('We got a barcode\n' +
+					  'Result: ' + result.text + '\n' +
+					  'Format: ' + result.format + '\n' +
+					  'Cancelled: ' + result.cancelled);
+
+				self.runtime.trigger(cr.plugins_.SimpleQRScanner.prototype.cnds.onDecoded, inst);
+			},
+			function (error) {
+				this.result = "";
+
+				alert('Scanning failed: ' + error);
+			},
+			{
+				preferFrontCamera : true, // iOS and Android
+				showFlipCameraButton : false, // iOS and Android
+				showTorchButton : true, // iOS and Android
+				torchOn: false, // Android, launch with the torch switched on (if available)
+				saveHistory: false, // Android, save scan history (default false)
+				prompt : 'Direcione a câmera para o QR Code', // Android
+				resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
+				formats : 'QR_CODE', // default: all but PDF_417 and RSS_EXPANDED
+				orientation : 'landscape', // Android only (portrait|landscape), default unset so it rotates with the device
+				disableAnimations : false, // iOS
+				disableSuccessBeep: false // iOS and Android
+			}
+		 );
+
+	};
+
+	pluginProto.acts = new Acts();
+	
+	//////////////////////////////////////
+	// Expressions
+	function Exps() {};
+	
+	// the example expression
+	/*Exps.prototype.MyExpression = function (ret)	// 'ret' must always be the first parameter - always return the expression's result through it!
+	{
+		ret.set_int(1337);				// return our value
+		// ret.set_float(0.5);			// for returning floats
+		// ret.set_string("Hello");		// for ef_return_string
+		// ret.set_any("woo");			// for ef_return_any, accepts either a number or string
+	};*/
+
+	Exps.prototype.decodedQRCode = function (ret)	// 'ret' must always be the first parameter - always return the expression's result through it!
+	{
+		ret.set_string(this.result);				// return our value
+		// ret.set_float(0.5);			// for returning floats
+		// ret.set_string("Hello");		// for ef_return_string
+		// ret.set_any("woo");			// for ef_return_any, accepts either a number or string
+	};
+	
+	// ... other expressions here ...
+	
+	pluginProto.exps = new Exps();
+
+}());
